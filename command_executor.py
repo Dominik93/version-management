cd6588f7f8f2c4a98901423a87a3b04f17a5b545
Dominik93 <slusarz.dominik@gmail.com>
2020-01-16 20:00:21 +0100

    VM-2 and fix logger

---
+++ command_executor.py
@@ -0,0 +1,21 @@
+import sys
+import os
+from logger import *
+
+
+class Cleaner:
+
+	logger = None
+
+	system = 'windows'
+
+	def __init__(self, system):
+		self.logger = Logger.getInstance()
+		self.system = system
+		self.logger.log("Init cleaner with " + self.system)
+
+	def clean(self, directory):
+		if self.system == 'windows':
+			os.system('rd /s /q "' + directory + '"')
+		else:
+			os.system('rm -r -f ' + directory)
